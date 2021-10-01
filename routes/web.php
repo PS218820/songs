6e4926e7f68cf6ac99bcafa7392b194c6e3556d7
PS218820 <74542567+PS218820@users.noreply.github.com>
2021-10-01 11:32:49 +0200

    Test

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/songs', function () {
+    return view('index');
+});
+
+Route::get('/songs/{index}', function () {
+    return view('show');
+});
+
+Route::get('/songs/create', function () {
+    return view('create');
+});
+
+Route::get('/songs/{index}/edit', function () {
+    return view('edit');
+});
